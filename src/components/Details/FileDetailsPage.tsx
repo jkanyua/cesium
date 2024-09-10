598ffa5886d7ff57662134c60a61d0089f129d65
Jimnah Magira <jimnah.magira@hotjar.com>
2024-09-10 19:26:12 +0300

    (feat): Add component structure - Add tailwind and tailwind config for styling - Add dumb app pages components and render temporarily in app - Cleanup directory and some more boilerplate code

---
+++ src/components/Details/FileDetailsPage.tsx
@@ -0,0 +1,42 @@
+export const FileDetailsPage = (): JSX.Element => {
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h2 className="text-2xl font-bold mb-4">File Records</h2>
+      <p className="mb-4">View details of uploaded CSV records</p>
+      <div className="overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* TODO: add file details mapper */}
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                1
+              </th>
+              <td className="px-6 py-4">John Doe</td>
+              <td className="px-6 py-4">john@example.com</td>
+              <td className="px-6 py-4">2024-09-10</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Home/HomePage.tsx
@@ -0,0 +1,17 @@
+export const HomePage = (): JSX.Element => {
+  return (
+    <div className="relative isolate overflow-hidden bg-blue-500 py-16 sm:py-24 lg:py-32">
+      <div className="mx-auto grid max-w-2xl ">
+        <div className="px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Welcome to home page.
+          </h2>
+          <p className="mt-4 text-lg leading-8 text-gray-300">
+            Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing
+            velit quis. Duis tempor incididunt dolore.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
