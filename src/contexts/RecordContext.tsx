f37c0efbd04598fdb93889358cfea52e47a103be
Jimnah Magira <jimnah.magira@hotjar.com>
2024-09-11 18:07:28 +0300

    (feat): Add details pagination and sorting

--- src/contexts/RecordContext.tsx
+++ src/contexts/RecordContext.tsx
@@ -8,21 +8,30 @@ interface UploadRecord {
   dateType: string;
 }
 
+export type SortField = keyof UploadRecord;
+type SortOrder = "asc" | "desc";
+
 interface RecordsContextType {
   records: UploadRecord[];
   addRecord: (record: UploadRecord) => void;
+  sortRecords: (field: SortField, order: SortOrder) => void;
+  currentPage: number;
+  setCurrentPage: (page: React.SetStateAction<number>) => void;
+  recordsPerPage: number;
+  setRecordsPerPage: (count: number) => void;
+  totalPages: number;
 }
 
 export const RecordsContext = createContext<RecordsContextType | undefined>(
   undefined
 );
 
-export const RecordsProvider = ({
+export const RecordsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
-}: {
-  children: React.ReactNode;
 }) => {
   const [records, setRecords] = useState<UploadRecord[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage, setRecordsPerPage] = useState(10);
 
   useEffect(() => {
     const storedRecords = localStorage.getItem("uploadRecords");
@@ -37,8 +46,33 @@ export const RecordsProvider = ({
     localStorage.setItem("uploadRecords", JSON.stringify(newRecords));
   };
 
+  const sortRecords = (field: SortField, order: SortOrder) => {
+    const sortedRecords = [...records].sort((a, b) => {
+      if (a[field] < b[field]) return order === "asc" ? -1 : 1;
+      if (a[field] > b[field]) return order === "asc" ? 1 : -1;
+      return 0;
+    });
+    setRecords(sortedRecords);
+  };
+
+  const totalPages = Math.ceil(records.length / recordsPerPage);
+
+  const contextValue: RecordsContextType = {
+    records: records.slice(
+      (currentPage - 1) * recordsPerPage,
+      currentPage * recordsPerPage
+    ),
+    addRecord,
+    sortRecords,
+    currentPage,
+    setCurrentPage,
+    recordsPerPage,
+    setRecordsPerPage,
+    totalPages,
+  };
+
   return (
-    <RecordsContext.Provider value={{ records, addRecord }}>
+    <RecordsContext.Provider value={contextValue}>
       {children}
     </RecordsContext.Provider>
   );
